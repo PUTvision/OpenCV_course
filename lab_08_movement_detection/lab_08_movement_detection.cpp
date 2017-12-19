d80d938c5d256f100f01b817cc6e2a9b5e8c0da7
Michał Fularz <michal.fularz@gmail.com>
2017-12-19 11:08:34 +0100

    Lab 8 updated and and aligned to use vcpkg

--- lab_08_movement_detection/lab_08_movement_detection.cpp
+++ lab_08_movement_detection/lab_08_movement_detection.cpp
@@ -1,161 +1,104 @@
-#include "opencv2/opencv.hpp"
+#include <opencv2/opencv.hpp>
 
-using namespace cv;
+// vcpkg OpenCV 3.3.X
 
-
-static void detectMovement(Mat &newFrame, Mat &background, Mat &mask, int thresh)
+void BackgroundSubtractorMOG2()
 {
-	Mat temp;
-	absdiff(newFrame, background, temp);
-	compare(temp, Scalar(thresh), mask, CMP_GE);
+	cv::Ptr<cv::BackgroundSubtractorMOG2> pMOG;
+	pMOG = cv::createBackgroundSubtractorMOG2();
+	cv::Mat currentFrameBGR;
+	cv::Mat foregroundFrame;
+
+	cv::VideoCapture capture(0);
+
+	const char windowCurrentFrame[] = "Current frame";
+	cv::namedWindow(windowCurrentFrame, cv::WINDOW_AUTOSIZE | cv::WINDOW_KEEPRATIO);
+
+	const char windowForegroundFrame[] = "Foreground frame";
+	cv::namedWindow(windowForegroundFrame, cv::WINDOW_AUTOSIZE | cv::WINDOW_KEEPRATIO);
+
+	int key = 'a';
+	while (key != 'q')
+	{
+		capture >> currentFrameBGR;
+		pMOG->apply(currentFrameBGR, foregroundFrame);
+
+		imshow(windowCurrentFrame, currentFrameBGR);
+		imshow(windowForegroundFrame, foregroundFrame);
+
+		key = cv::waitKey(1);
+	}
 }
 
-static void updateBackground(Mat& frame, Mat& background, Mat& isForeground, int method, double alpha)
+static void detectMovement(const cv::Mat &newFrame, const cv::Mat &background, const cv::Mat &mask, int thresh)
 {
-	Mat isBackground = Mat::ones(isForeground.size(), CV_8UC1) * 255 - isForeground;
+	cv::Mat temp;
+	absdiff(newFrame, background, temp);
+	compare(temp, cv::Scalar(thresh), mask, cv::CMP_GE);
+}
+
+enum class Method { SIMPLE, AVG_MEDIAN, ACCUMULATE_WEIGHTED };
+
+static void updateBackground(const cv::Mat &frame, cv::Mat &background, const cv::Mat &isForeground, Method method, double alpha)
+{
+	cv::Mat isBackground = cv::Mat::ones(isForeground.size(), CV_8UC1) * 255 - isForeground;
 	switch (method)
 	{
-	case 0: //last frame
-		frame.copyTo(background);
-		break;
-	case 1: //moving average
-
-		accumulateWeighted(frame, background, alpha);
-
-
-		break;
-
-	case 2:
-		Mat mask1 = (frame>background) / 255;
-		Mat mask2 = (frame<background) / 255;
-		add(background, mask1, background, isBackground, CV_32FC1);
-		subtract(background, mask2, background, isBackground, CV_32FC1);
-		break;
-
-	}
-
-}
-
-enum method_t { m_simple, m_avg_median, m_accumulateWeighted };
-
-static void myObjectDetect(void)
-{
-	Mat currentFrame;
-	Mat background;
-	Mat foreground;
-
-	const int numberOfImages = 1147; // 283;
-	const char fileNamePattern[] = "C://obrazy//sekwencja//p_%04d.pgm"; // "C:/segm1_png/image%03d.png";
-
-	char filename[100];
-	sprintf(filename, fileNamePattern, 1);
-
-	background = imread(filename, IMREAD_GRAYSCALE);
-
-	method_t method = m_avg_median;//m_accumulateWeighted;
-
-	const char windowCurrentFrame[] = "currentFrame";
-	const char windowBackground[] = "background";
-	const char windowForeground[] = "foreground";
-
-	for (int i = 2; i<numberOfImages; ++i)
-	{
-		sprintf(filename, fileNamePattern, i);
-		currentFrame = imread(filename, IMREAD_GRAYSCALE);
-
-		absdiff(currentFrame, background, foreground);
-
-		Mat temp = foreground.clone();
-		blur(foreground, temp, Size(9, 9));
-		dilate(temp, foreground, Mat(), Point(-1, -1), 5);
-
-		threshold(foreground, foreground, 15, 255, THRESH_BINARY);
-
-		imshow(windowCurrentFrame, currentFrame);
-		imshow(windowBackground, background);
-		imshow(windowForeground, foreground);
-
-		waitKey(50);
-
-		if (m_simple == method)
-		{
-			background = currentFrame.clone();
-		}
-		else if (m_avg_median == method)
-		{
-			uchar updateValue = 1;
-
-			for (int j = 0; j<currentFrame.rows; ++j)
-			{
-				uchar* backgroundCurrentRow = background.ptr<uchar>(j);
-				const uchar* currentFrameCurrentRow = currentFrame.ptr<uchar>(j);
-
-				for (int k = 0; k<currentFrame.cols; ++k)
-				{
-					if (backgroundCurrentRow[k] > currentFrameCurrentRow[k])
-					{
-						backgroundCurrentRow[k] -= updateValue;
-					}
-					else if (backgroundCurrentRow[k] < currentFrameCurrentRow[k])
-					{
-						backgroundCurrentRow[k] += updateValue;
-					}
-				}
-			}
-		}
-		else if (m_accumulateWeighted == method)
-		{
-			Mat backgroundF;
-			background.convertTo(backgroundF, CV_32F);
-			Mat currentFrameF;
-			currentFrame.convertTo(currentFrameF, CV_32F);
-
-			accumulateWeighted(currentFrameF, backgroundF, 0.2);
-
-			backgroundF.convertTo(background, CV_8U);
-			currentFrameF.convertTo(currentFrame, CV_8U);
-		}
+		case Method::SIMPLE: //last frame
+			frame.copyTo(background);
+			break;
+		case Method::ACCUMULATE_WEIGHTED: //moving average
+			accumulateWeighted(frame, background, alpha);
+			break;
+		case Method::AVG_MEDIAN:
+			cv::Mat maskAdd = (frame>background) / 255;
+			cv::Mat maskSubtract = (frame<background) / 255;
+			add(background, maskAdd, background, isBackground, CV_32FC1);
+			subtract(background, maskSubtract, background, isBackground, CV_32FC1);
+			break;
 	}
 }
 
 static void ObjectTracking(void)
 {
+	cv::VideoCapture cap(0);// "..\\data\\video\\dt_passat.mpg");
 
-	char filename[1000];
+	cv::Mat background;
+	cv::Mat isForeground;
+	cv::Mat newFrame;
+	cv::Mat newFrameGray;
+	cv::Mat newFrameF;
+	cap >> newFrame;
+	cvtColor(newFrame, newFrameGray, cv::COLOR_BGR2GRAY);
+	newFrameGray.convertTo(background, CV_32FC1);
 
-	Mat background;
-	Mat isForeground;
-	Mat newFrame;
-	Mat newFrameGray;
-	Mat newFrameF;
-	sprintf(filename, "C:/segm1_png/image%03d.png", 1);
-	newFrame = imread(filename, 0);
-	newFrame.convertTo(background, CV_32FC1);
+	isForeground = cv::Mat::zeros(background.size(), CV_8UC1);
 
-	isForeground = Mat::zeros(background.size(), CV_8UC1);
-
-
-
-	for (int i = 2; i<283; i++)
+	for (;;)
 	{
-		sprintf(filename, "C:/segm1_png/image%03d.png", i);
-		newFrame = imread(filename);
-		cvtColor(newFrame, newFrameGray, COLOR_BGR2GRAY);
+		cap >> newFrame;
+		cvtColor(newFrame, newFrameGray, cv::COLOR_BGR2GRAY);
+		// this line is used instead of cvtColor when working on grayscale video
+		//newFrame.copyTo(newFrameGray);
 		newFrameGray.convertTo(newFrameF, CV_32FC1);
 
 		detectMovement(newFrameF, background, isForeground, 30);
 
-		updateBackground(newFrameF, background, isForeground, 1, 0.3);
+		updateBackground(newFrameF, background, isForeground, Method::AVG_MEDIAN, 0.3);
 		imshow("image", newFrame);
 		imshow("movement", isForeground);
 		imshow("background", background / 255.);
-		waitKey(50);
-
+		cv::waitKey(50);
 	}
+
+	cap.release();
 }
 
-int main(int, char)
+int main(void)
 {
-	// the camera will be deinitialized automatically in VideoCapture destructor
-	return 0;
+	ObjectTracking();
+
+	BackgroundSubtractorMOG2();
+
+	return 1;
 }
